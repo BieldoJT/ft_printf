17cb6c371f3aae2aa4427c511475bdc81e7135f9
gda-conc <gabrieldaconceicaodeoliveira@gmail.com>
2024-10-16 11:48:21 -0300

    coloquei o retorno do tamanho da str e mudei o nome do arquivo

---
+++ print_str.c
@@ -0,0 +1,22 @@
+#include "libftprintf.h"
+
+int	ft_print_str(char *str)
+{
+	ft_putstr_fd(str,1);
+	return (ft_strlen(str));
+}
+/*
+#include <stdio.h>
+
+int main()
+{
+	char *string = "";
+
+	ft_print_str(string); //testando se está printando a string.
+	printf("\n\n\n%s",string); // usando a printf.
+
+	printf("\n\n\nvalue of ft_printf: %d",ft_print_str(string));
+	printf("\n\n\nvalue of original printf: %d",printf("%s",string));
+
+	return(0);
+}*/
