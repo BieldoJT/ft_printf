361a1f621047a83acb774faf5bd2d27b9e03dc0f
BieldoJT <gabrieldaconceicaodeoliveira@gmail.com>
2024-10-16 21:56:48 -0300

    fiz a print_nbr

--- print_nbr.c
+++ print_nbr.c
@@ -1,3 +1,27 @@
 #include "libftprintf.h"
 
-print_nbr()
+int	print_nbr(int n)
+{
+	int		len;
+	char	*num;
+
+	len = 0;
+	num = ft_itoa(n);
+	len = print_str(num);
+	return (len);
+}
+
+
+
+#include <limits.h>
+
+int main()
+{
+	int num  = INT_MIN;
+
+	print_nbr(num);
+	printf("\n");
+	printf("\nprintando o tamnho da str: %d\n",print_nbr(num)); // printando o tamanho da str
+
+	return (0);
+}
